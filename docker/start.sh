a6bb5de688e62ba47659842f5667539c3df04d80
Adam McMaster <adam@zooniverse.org>
2015-02-13 15:58:18 +0000

    Run under supervisor and add auto-ID option

---
+++ docker/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ex
+
+if [ "$MYID" == "auto" ]
+then
+    # Use the last octet of the IP address for the ID. You'll probably want
+    # to start the container with --net=host and put all the nodes in the
+    # same subnet.
+    MYID=$(hostname -i | awk -F. '{print$4}')
+fi
+
+if [ ! -z "$MYID" ]
+then
+    echo $MYID > /etc/zookeeper/conf/myid
+fi
+
+exec /usr/bin/supervisord
