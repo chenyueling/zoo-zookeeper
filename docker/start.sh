4ff1f93d82361a6d0ebbfea77141c496472e3d73
Adam McMaster <adam@zooniverse.org>
2015-02-17 15:05:45 +0000

    Fix auto ID when hostname doesn't resolve

--- docker/start.sh
+++ docker/start.sh
@@ -5,7 +5,7 @@ then
     # Use the last octet of the IP address for the ID. You'll probably want
     # to start the container with --net=host and put all the nodes in the
     # same subnet.
-    MYID=$(hostname -i | awk -F. '{print$4}')
+    MYID=$(hostname -I | awk "-F " '{print$1}' | awk -F. '{print$4}')
 fi
 
 if [ ! -z "$MYID" ]
